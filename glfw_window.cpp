d33bf5776779299312b4c5d94b90e732e7aeb4f8
Certified Humane <castrolwilliam@gmail.com>
2022-09-27 10:03:32 -0400

    Update glfw_window.cpp

--- glfw_window.cpp
+++ glfw_window.cpp
@@ -1,1 +1,28 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
 
+
+
+int main(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3 );
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_ANY_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW",NULL,NULL);
+    if (window == NULL){
+        std::cout << "Failed to open GLFW WINDOW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    
+    while (!glfwWindowShouldClose(window)){
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
