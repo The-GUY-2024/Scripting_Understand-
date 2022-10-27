1bf5c34fa0f35d177c0899e28f758df7acae3f04
Certified Humane <castrolwilliam@gmail.com>
2022-10-27 12:18:59 -0700

    Add files via upload

---
+++ simple_Cmake/simple_example.cpp
@@ -0,0 +1,9 @@
+
+#include "simple_lib.hpp"
+#include <iostream>
+
+int main(){
+    std::cout << "Simple example " << std::endl;
+    std::cout << simple_lib_function() << std::endl;
+    return 0;
+}
---
+++ simple_Cmake/simple_lib.hpp
@@ -0,0 +1,4 @@
+#pragma once 
+#include <string>
+
+std::string simple_lib_function();
